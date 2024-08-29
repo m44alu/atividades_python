90b6a996ab7551e9db39320a9ffb53263b9a0420
m44alu <adrinelmamelo@gmail.com>
2024-08-29 11:51:15 -0300

    Add files via upload

---
+++ 03aula.py
@@ -0,0 +1,16 @@
+#variaveis em python
+#nao ha necessidade de declara-las no inicio do programa
+# basta criar no meio do algoritmo com a atribuiçao
+
+teste = "maria luiza"
+print(teste) # imprime a string "maria luiza"
+print(type(teste)) # imprime o tipo da variavel, string
+teste = 10
+print(teste) # imprime o numero 10
+print(type(teste)) # imprime o tipo da variavel, que é int
+teste = 1.56
+print(teste)#imprime o numero 1.74
+print(type(teste))#imprime o tipo da variavel, que é float
+teste = True
+print(teste) # imprime o valor true
+print(type(teste)) # imprime o tipo da variavel, que é bool
---
+++ 04nome-idade.py
@@ -0,0 +1,14 @@
+# atribuicao de variaveis
+nome = "maria luiza"
+idade = 16
+altura = 1.56
+
+# exibir mensagem na tela
+print("nome; ", nome, " idade; ", idade, "altura; ", altura)
+# formatacao com f-string
+print(f"nome: {nome}, idade: {idade}, altura: {altura}")
+
+nome = input("digite o seu nome; ")
+idade = int(input("digite a sua idade; "))
+altura = float(input("digite a sua altura; "))
+print(f"nome: {nome}, idade: {idade}, altura: {altura}")
