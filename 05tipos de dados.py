5c56f92ca3d9d5c9ea8326054ef11a56de6fcc94
m44alu <adrinelmamelo@gmail.com>
2024-09-10 10:48:41 -0300

    Add files via upload

---
+++ 05tipos de dados.py
@@ -0,0 +1,12 @@
+# tipos de dados em python
+# inteiros
+inteiro = 10
+print(inteiro)
+# float
+float = 10.5
+print(float)
+# string
+string = "Olá, mundo"
+print(string)
+# booleanos
+booleano = True
